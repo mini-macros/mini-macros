833423f11c4d841ed2767c519c5fae1760d4fc1b
Jack Gaskins <jackgaskins.tech@gmail.com>
2026-03-07 17:37:50 -0500

    edit linting config, create Macro component
    prod based linting. basic Macro structure

---
+++ frontend/src/components/Macro.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const styles = "p-20 m-10 border-black";
+
+// TODO: add props for server side
+interface MacroProps {
+  title: string;
+  content: string;
+}
+
+function Macro({ title, content }: MacroProps) {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+    navigator.clipboard
+      .writeText(content)
+      .then(() => {
+        // TODO: add success/fail component for UI
+        console.log("Copied successfully!");
+      })
+      .catch((err) => {
+        console.log("Failed to copy", err);
+      });
+  }
+
+  return (
+    <div className={styles}>
+      <button onClick={handleClick}>{title}</button>
+    </div>
+  );
+}
+
+export default Macro;
